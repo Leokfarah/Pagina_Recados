5adb22a1f2d04ebdbbb82c3af2514ca5d1341105
Leokfarah <leonardo12farah@outlook.com>
2023-01-14 16:42:06 -0300

    hook chenge useLayoutEffect -> useEffect

--- src/components/headerArquivados/HeaderArquivados.tsx
+++ src/components/headerArquivados/HeaderArquivados.tsx
@@ -13,7 +13,7 @@ export const HeaderArquivados = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
-    React.useLayoutEffect(() => {
+    React.useEffect(() => {
         const userId = loginData.dados.at(0);
 
         const dadosBusca = {
