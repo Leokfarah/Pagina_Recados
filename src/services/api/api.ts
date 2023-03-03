2506139a8b9b358f58cf72d73263fe212e1d32a8
Leokfarah <leonardo12farah@outlook.com>
2023-03-03 19:42:03 -0300

    alterada conexão de local para online

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const online = 'https://api-recados.vercel.app/';
-const local = 'http://localhost:8080';
+const online = 'https://api-recados2.vercel.app/';
+// const local = 'http://localhost:8080';
 
 export const api = axios.create({
-    // baseURL: online,
-    baseURL: local,
+    baseURL: online,
+    // baseURL: local,
     headers: {
         "Content-type": "application/json",
     },
